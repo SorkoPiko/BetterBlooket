3a7300c38ffb123f34f5e35cfb2e08304f925441
SorkoPiko <36188446+SorkoPiko@users.noreply.github.com>
2023-07-21 10:50:16 +1000

    re-add mac fix

--- vite.config.js
+++ vite.config.js
@@ -18,8 +18,8 @@ export default defineConfig(async () => ({
   envPrefix: ["VITE_", "TAURI_"],
   build: {
     // Tauri supports es2021
-    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
-    //target: "esnext",
+    //target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+    target: "esnext",
     // don't minify for debug builds
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     // produce sourcemaps for debug builds
